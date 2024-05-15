91d2488d867b08befcffd30e09ffa063241ec4eb
sagnik@work <sagnikchatterjee607@gmail.com>
2024-05-15 13:23:38 +0530

    src: fix orderItemROuter file name

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -9,6 +9,8 @@ import (
 func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
 	incomingRoutes.GET("/orderItems/:order_item_id", controller.GetOrderItem())
+	//get all the order items inside the router
+	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
 	incomingRoutes.POST("/orderItems", controller.CreateOrderItems())
 	incomingRoutes.PATCH("/orderItems/:order_item_id", controller.UpdateOrderItem())
 }
